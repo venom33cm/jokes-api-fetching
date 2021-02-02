a3d66f426fd0b6bbabd362e97aa0c1dc37b19985
venom33cm <cmyash36@gmail.com>
2021-02-02 19:23:58 +0530

    jokes api

---
+++ index.js
@@ -0,0 +1,26 @@
+// const span = document.querySelector('.jokes')
+const btn = document.querySelector('.btn');
+const getjokes = async()=>{
+    try{
+        span.innerHTML="loading....";
+        setTimeout(async()=>{
+        const setheader = {
+        headers: {
+            Accept : "application/json"
+        }
+    }
+    const res = await fetch( "https://icanhazdadjoke.com",setheader)
+    const data = await res.json();
+    span.innerHTML=data.joke;
+    console.log(data)
+},500)
+    }
+    catch(err){
+        console.log(err);
+
+    }
+  
+
+}
+btn.addEventListener('click',getjokes)
+getjokes();
